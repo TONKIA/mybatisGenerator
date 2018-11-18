3592c0cf06c304e6a139bc3f6e1f80f05c6e0a2c
tonkia <tongxk96@outlook.com>
2018-11-18 11:23:04 +0800

    init

---
+++ src/com/coder/mapper/UserMapper.java
@@ -0,0 +1,22 @@
+package com.coder.mapper;
+
+import com.coder.pojo.User;
+import com.coder.pojo.UserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserMapper {
+    long countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+}
